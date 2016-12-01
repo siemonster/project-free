d0c1c278fed02d0315c87b2d2779016c365cc213
Alex Mourzina <alexandramourzina@gmail.com>
2016-12-01 22:34:56 +0100

    fixed first script for dbinit

--- dbinit_remote.js
+++ dbinit_remote.js
@@ -6,7 +6,11 @@ if(!process.env.MSA_SKEY || !process.env.MSA_URL || !process.env.SITE_NAME || !p
 }
 
 request.post(
-    { url: process.env.MSA_URL, json: true, form: { key: process.env.MSA_SKEY, sitename: process.env.SITE_NAME, refname: process.env.SITE_REF_NAME } }
+    {
+          url : process.env.MSA_URL + '/api/admin/site_clone'
+        , json: true
+        , body: { api_key: process.env.MSA_SKEY, sitename: process.env.SITE_NAME, refname: process.env.SITE_REF_NAME }
+    }
     , function (err, res, body) {
         console.log("Error:" + err);
         console.log("Body:"  + JSON.stringify(res.body));
--- server.js
+++ server.js
@@ -2,11 +2,25 @@ var express = require('express');
 var app = express();
 var compress = require('compression');
 var fs = require('fs');
+var exec = require('child_process').exec
 
 var port = parseInt(process.env.PORT) || 80;
 var http = require('http');
 
+var checkfn = 'dbinitcheck.txt';
+
 app.use(compress());
+
+if(!fs.existsSync(checkfn)) {
+    exec('node dbinit_remote.js', function(err) {
+        if(!err) {
+            fs.writeFileSync(checkfn, 'true');
+            return;
+        }
+
+        console.log(err);
+    });
+}
 
 app.get('/', function(req, res) {
     fs.readFile('./domain/index.html', function (err, data) {
@@ -21,9 +35,7 @@ app.get('/health/', function(req, res) {
     res.json({health: "ok"});
 });
 
-console.log('__dirname = ' + __dirname);
 app.use(express.static(__dirname + '/'));
-
 app.listen(port);
 
 console.log('Listening on port ' + port);
