6bb2b7a3e3f1ce3681d1898f97fabe3297b0224f
Alex Mourzina <alexandramourzina@gmail.com>
2016-12-02 01:33:19 +0100

    added domain site name to dbiinit script

--- dbinit_remote.js
+++ dbinit_remote.js
@@ -1,6 +1,6 @@
 const request = require('request');
 
-if(!process.env.MSA_SKEY || !process.env.MSA_URL || !process.env.SITE_NAME || !process.env.SITE_REF_NAME) {
+if(!process.env.MSA_SKEY || !process.env.MSA_URL || !process.env.SITE_NAME || !process.env.SITE_REF_NAME || !process.env.SITE_DNAME) {
     console.log('You have to specify site name, MSA secret key and url for requesting db init.');
     return;
 }
@@ -9,7 +9,7 @@ request.post(
     {
           url : process.env.MSA_URL + '/api/admin/site_clone'
         , json: true
-        , body: { api_key: process.env.MSA_SKEY, sitename: process.env.SITE_NAME, refname: process.env.SITE_REF_NAME }
+        , body: { api_key: process.env.MSA_SKEY, sitename: process.env.SITE_NAME, refname: process.env.SITE_REF_NAME, dname: process.env.SITE_DNAME }
     }
     , function (err, res, body) {
         console.log("Error:" + err);
