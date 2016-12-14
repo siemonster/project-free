f1b0a8e3c6c2de83b01e78b8215061ed89b07cf5
Alexandra Murzina <alexandramourzina@gmail.com>
2016-12-14 01:23:02 +0100

    fix containers

--- containers/411/wrap_startup.sh
+++ containers/411/wrap_startup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-sqlite3 /data/data.db "update sites set host='$thishost' where site_id=1;"
+sqlite3 /data/data.db "update sites set host='$HOST' where site_id=1;"
+
 exec /bin/sh -c /startup.sh
