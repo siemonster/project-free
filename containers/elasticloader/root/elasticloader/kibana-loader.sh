3b31091e372a3c86a2494bee7dd5e9719beb7233
siemonster <siemonster@users.noreply.github.com>
2017-01-02 12:20:33 +1100

    Update loader for dockbeat

--- containers/elasticloader/root/elasticloader/kibana-loader.sh
+++ containers/elasticloader/root/elasticloader/kibana-loader.sh
@@ -8,3 +8,4 @@ elasticdump --input=bro-mapping.json --output=http://es-client:9200/@bro-meta --
 elasticdump --input=bro-data.json --output=http://es-client:9200/@bro-meta --type=data
 elasticdump --input=kibi-mapping.json --output=http://es-client:9200/.kibi --type=mapping
 elasticdump --input=kibi-data.json --output=http://es-client:9200/.kibi --type=data
+curl -XPUT 'http://es-client:9200/_template/dockbeat' -d@dockbeat.template.json
