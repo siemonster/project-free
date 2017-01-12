f4c973dd5fe397f505b1119f168373cfacc3003b
siemonster <siemonster@users.noreply.github.com>
2017-01-12 16:00:56 +1100

    Kibana update

--- containers/elasticloader/root/elasticloader/kibana-loader.sh
+++ containers/elasticloader/root/elasticloader/kibana-loader.sh
@@ -6,8 +6,8 @@ done
 
 elasticdump --input=bro-mapping.json --output=http://es-client:9200/@bro-meta --type=mapping
 elasticdump --input=bro-data.json --output=http://es-client:9200/@bro-meta --type=data
-elasticdump --input=kibana-mapping.json --output=http://es-client:9200/.kibi --type=mapping
-elasticdump --input=kibana-data.json --output=http://es-client:9200/.kibi --type=data
+elasticdump --input=kibana-mapping.json --output=http://es-client:9200/.kibana --type=mapping
+elasticdump --input=kibana-data.json --output=http://es-client:9200/.kibana --type=data
 curl -XPUT 'http://es-client:9200/_template/dockbeat' -d@dockbeat.template.json
 curl -XDELETE 'http://es-client:9200/dockbeat*'
 curl 'http://es-client:9200/_template/cisco-fw' -d@cisco-fw-mapping.json
